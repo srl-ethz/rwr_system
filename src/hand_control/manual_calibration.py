21549ec0295ca0a803c23644a583dc850432776c
maximilianeberlein <meberlein@student.ethz.ch>
2024-11-25 14:32:27 +0100

    fix hand_defs.yaml values for latest  hardware setup

--- src/hand_control/manual_calibration.py
+++ src/hand_control/manual_calibration.py
@@ -2,5 +2,4 @@ import time
 from faive_system.src.hand_control.hand_controller import HandController
 
 hc = HandController("/dev/ttyUSB0", calibration = True)
-hc.connect_to_dynamixels()
 time.sleep(3.0)
